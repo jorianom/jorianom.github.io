15b3e73b635b20e02cc136d13827b2ad655b2007
John Riaño <jorianom@unal.edu.co>
2026-03-24 21:59:38 -0500

    fix: update basepath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-export const basePath = process.env.NODE_ENV === 'production' ? '/me' : '';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: basePath,
   images: { unoptimized: true },
 };
 
